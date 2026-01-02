e7e43bd458af9f5fe2d6b99da794325703a56ff3
Yuga Umemura <yuga0211ume@gmail.com>
2026-01-02 14:51:50 +0900

    context provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./globals.css";
 import Link from "next/link";
+import ThemeProvider from "theme-provider";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <header className="px-4 py-4 bg-fuchsia-800 text-white shadow">
-          <nav className="flex gap-4">
-            {/* Prefetched when the link is hovered or enters the viewport */}
-            <Link href="/blog">Blog</Link>
-            {/* no prefetching */}
-            <a href="/contact">Contact</a>
-          </nav>
-        </header>
-        {/* Place children where you want to render a page or nested layout */}
-        <main className="px-4 py-4">{children}</main>
+        <ThemeProvider>
+          <header className="px-4 py-4 bg-fuchsia-800 text-white shadow">
+            <nav className="flex gap-4">
+              {/* Prefetched when the link is hovered or enters the viewport */}
+              <Link href="/blog">Blog</Link>
+              {/* no prefetching */}
+              <a href="/contact">Contact</a>
+            </nav>
+          </header>
+          {/* Place children where you want to render a page or nested layout */}
+          <main className="px-4 py-4">{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ app/theme-provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { createContext } from "react";
+
+export const ThemeContext = createContext({});
+
+export default function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <ThemeContext.Provider value="dark">{children}</ThemeContext.Provider>;
+}
